918d27422a74aa3449bcd99f94077bdc35809db6
Kirill Kovalev <kirillkovalevv06@gmail.com>
2023-11-07 14:36:22 +0300

    Подготовительные работы перед миграцией, созданиме Models, Controllers. Установка NuGet пакетов

--- myRockyMVC/Program.cs
+++ myRockyMVC/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace myRockyMVC
+namespace mymyRockyMVCMVC
 {
     public class Program
     {
--- myRockyMVC/Startup.cs
+++ myRockyMVC/Startup.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace myRockyMVC
+namespace mymyRockyMVCMVC
 {
     public class Startup
     {
